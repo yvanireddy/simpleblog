93474c168ee80a9d9df25d7679cae5b59145f6cf
vanireddy592 <vimala.vani123@gmail.com>
2017-08-02 06:31:49 -0700

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+def about
+	@title= 'about us'
+	@content = 'content about page'
+end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+http_basic_authenticate_with name: "vani", password:"vani" ,except:[:index, :show]
+
+
+def index
+@posts=Post.all
+end
+def show 
+	@post=Post.find(params[:id])
+end
+
+def new
+@post=Post.new
+
+end
+
+def edit 
+	@post=Post.find(params[:id])
+
+end
+def update
+
+@post=Post.find(params[:id])
+if(@post.update(post_params))
+redirect_to @post
+else 
+	render 'edit'
+end
+
+
+end
+
+def create
+#render plain: params[:post].inspect
+@post=Post.new(post_params)
+if(@post.save)
+redirect_to @post
+else 
+	render 'new'
+end
+end
+
+def destroy
+	@post=Post.find(params[:id])
+	@post.destroy
+
+	redirect_to posts_path
+	end
+
+private def post_params
+params.require(:post).permit(:title, :body)
+end
+end
+
